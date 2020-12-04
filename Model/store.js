ca0e36ccfb4f458b6280eeb9a9e1e3967aa3ffc5
TheMachine <themachine@pop-os.localdomain>
2020-12-03 23:37:33 -0600

    :heavy_plus_sign: antes del push 3

--- Model/store.js
+++ Model/store.js
@@ -22,23 +22,46 @@ for (let index = 0; index < carro.length; index++) {
         console.log("added to cart");
     })
 }
+//FUNCION CARGAR EL CARRITO
+function onLoadCartNumbers(){
+    let productNumbers = localStorage.getItem('cartNumbers');
+    //Product Numbers
+    if(productNumbers){
+        document.querySelector('.cart span').textContent = productNumbers;
+    }
+}
+
 //FUNCION PARA AGREGAR AL CARRITO DE COMPRAS
-function carritoNumeros(){
-    let productoNumber = localStorage.setItem('carritoNumeros',1);
+function cartNumbers(){
+    let productoNumber = localStorage.setItem('carritoNumeros');
 
     productoNumbers = parseInt(productoNumbers);
-    localStorage.setItem('')
+//IF PRODUCTO NUMBER 
+    if(productoNumber){
+        productoNumber = parseInt(productoNumber);
+        localStorage.setItem('carritoNumeros', productoNumber + 1);
+        document.querySelector('.carrito span').textContent = productoNumber + 1;
+    } else {
+        localStorage.setItem('carritoNumeros',1);
+        document.querySelector('.carrito span').textContent = 1;
+    }
 }
 
 //FUNCION DE REMOVER COSAS DEL CARRITO DE COMPRAS
 var removeCartItemButtons = document.getElementsByClassName('elqueleguaponer');
 console.log(removeCartItemButtons);
 for (let index = 0; index < removeCartItemButtons; index++) {
-    const button = removeCartItemButtons[index];   
+    const button = removeCartItemButtons[index];
+    //STEMEN   
     button.addEventListener("click",function(event){
         var buttonClicked = event.target;
         buttonClicked.parentElement.parentElement.remove();
     })
+}
+
+//FUncon de objetos
+function setItems(product){
+    product.inca
 }
 
 
