c48aca80c25ac10dd686ae73bc4be1a7e0e7bdb8
TheMachine <themachine@pop-os.localdomain>
2020-12-04 03:59:18 -0600

    :heavy_plus_sign: antes del push 4

---
+++ Model/JSON/crema.js
@@ -0,0 +1,62 @@
+let cremas = [
+    {
+        nombre : 'Crema LALA',
+        precio : '5.00 ',
+        enCarrito : '0'
+    },
+    {
+        nombre : 'Crema yoplait',
+        precio : '4.50',
+        enCarrito : '0'
+    },
+    {
+        nombre : 'Crema Milkaut',
+        precio : '6.00  ',
+        enCarrito : '0'
+    },
+    {
+        nombre : 'Crema Milkaut pasteurizada',
+        precio : '7.00',
+        enCarrito : '0'
+    },
+    {
+        nombre : '  Crema LALA',
+        precio : '2.50',
+        enCarrito : '0'
+    },
+    {
+        nombre : ' Crema Nuestra Crema',
+        precio : '3.90',
+        enCarrito : '0'
+    },
+    {
+        nombre : ' Crema El Huache',
+        precio : '5.50',
+        enCarrito : '0'
+    },
+    {
+        nombre : ' Crema Philadelphia',
+        precio : '4.50',
+        enCarrito : '0'
+    },
+    {
+        nombre : 'Crema Carabobo',
+        precio : '3.00',
+        enCarrito : '0'
+    },
+    {
+        nombre : ' Crema alpura',
+        precio : '5.00',
+        enCarrito : '0'
+    },
+    {
+        nombre : ' Crema sour cream',
+        precio : '3.70',
+        enCarrito : '0'
+    },
+    {
+        nombre : ' Crema Quillayes',
+        precio : '3.95 ',
+        enCarrito : '0'
+    }
+]
---
+++ Model/JSON/leche.js
@@ -0,0 +1,62 @@
+let leches = [
+    {
+        nombre: 'Leche Lactaid',
+        precio: '3.70 ',
+        enCarrito: '0'
+    },
+    {
+        nombre: 'Leche nutRi',
+        precio: '4.75',
+        enCarrito: '0'
+    },
+    {
+        nombre: 'Leche Bienatur',
+        precio: '3.50  ',
+        enCarrito: '0'
+    },
+    {
+        nombre: 'Leche sula Entera ',
+        precio: '3.50',
+        enCarrito: '0'
+    },
+    {
+        nombre: '  Leche Nestle',
+        precio: '3.95',
+        enCarrito: '0'
+    },
+    {
+        nombre: '  Leche sula Deslactosada',
+        precio: '3.00',
+        enCarrito: '0'
+    },
+    {
+        nombre: '  Leche Great Value',
+        precio: '5.00',
+        enCarrito: '0'
+    },
+    {
+        nombre: '  Leche LALA',
+        precio: '4.00',
+        enCarrito: '0'
+    },
+    {
+        nombre: '  Leche Pascual',
+        precio: '5.00',
+        enCarrito: '0'
+    },
+    {
+        nombre: ' Leche President',
+        precio: '7.00',
+        enCarrito: '0'
+    },
+    {
+        nombre: '  Leche Serenísima',
+        precio: '5.00',
+        enCarrito: '0'
+    },
+    {
+        nombre: ' Leche Fortuna',
+        precio: '5.00',
+        enCarrito: '0'
+    }
+]
